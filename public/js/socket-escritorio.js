8d77fcfae8db990f99d6a1d668c6dd6f444df283
Eduardo Torrez <38381517+rucko18@users.noreply.github.com>
2018-12-08 15:46:35 -0600

    Primer commit

---
+++ public/js/socket-escritorio.js
@@ -0,0 +1,26 @@
+// Comando para estableser la conexion con socket.io
+
+var socket = io();
+
+var searchParams = new URLSearchParams(window.location.search);
+if(!searchParams.has('escritorio')){
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+var label = $('small');
+console.log(escritorio);
+
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function(){
+    socket.emit('atenderTicket', { escritorio: escritorio}, function(res){
+        if (res == 'No hay Tickets') {
+            label.text(res);
+            alert(res);
+            return;
+        }
+        label.text('Ticket: '+res.numero);
+    });
+});
---
+++ public/js/socket-new-ticket.js
@@ -0,0 +1,23 @@
+// Comando para estableser la conexion con socket.io
+
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function(){
+    console.log('Conectado al servidor'); 
+});
+
+socket.on('disconnect', function(){
+    console.log('Desconectado del servidor'); 
+});
+
+socket.on('estadoActual', function(res){
+    label.text(res.actual);
+});
+
+$('button').on('click', function(){
+    socket.emit('siguienteTicket', null, function(siguienteTicket){
+        label.text(siguienteTicket);
+    });
+});
